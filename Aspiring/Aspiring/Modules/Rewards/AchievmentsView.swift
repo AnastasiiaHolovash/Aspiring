e22bf130947a8c9d8a2f3afe542f5f6598782123
Yelyzaveta Kartseva <kartcevayelyzaveta@gmail.com>
2022-09-18 14:13:01 +0300

    added achievement items

--- Aspiring/Aspiring/Modules/Rewards/AchievmentsView.swift
+++ Aspiring/Aspiring/Modules/Rewards/AchievmentsView.swift
@@ -5,4 +5,34 @@
 //  Created by Yelyzaveta Kartseva on 18.09.2022.
 //
 
-import Foundation
+import SwiftUI
+
+struct AchievmentsView: View {
+
+    @State var achievments: [Achievment] = []
+
+    let columns = [
+        GridItem(.adaptive(minimum: 187))
+    ]
+
+    var body: some View {
+        ZStack {
+            Color.clear.ignoresSafeArea()
+            ScrollView {
+                LazyVGrid(columns: columns, spacing: 20) {
+                    ForEach(achievments) { item in
+                        AchievmentView(achievment: item)
+                    }
+                }
+            }
+        }
+        .frame(maxHeight: .infinity, alignment: .top)
+    }
+
+}
+
+struct AchievmentsVieww_Previews: PreviewProvider {
+    static var previews: some View {
+        AchievmentsView(achievments: userAchievments)
+    }
+}
